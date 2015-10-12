d75dcee9a4d5bb1f59d1e2e27ca8d6590526b9c7
Matt Morris <mcm0027@yahoo.com>
2015-10-11 19:04:29 -0500

    fix error add favicon

--- js/app.js
+++ js/app.js
@@ -13,11 +13,14 @@ function newGame() {
 	console.log(randomNum);
 	$("#guessList").children().remove();
 	numGuesses = 0;
+	diffGuess =0;
 	$("#count").text(numGuesses);
 	$("#feedback").text("Make your Guess!");
 }
 
 function guessResponse(guess){
+	console.log("diff guess "+ diffGuess);
+		console.log("old diff "+ oldDiff);
 	if (numGuesses === 1) {
 		if (guess == randomNum) {
 		 	$(".win").fadeIn(1000);
@@ -36,23 +39,50 @@ function guessResponse(guess){
 			$("#feedback").text("You are very hot!")
 		}
 	} else {
+
 		if (guess == randomNum) {
 			$(".win").fadeIn(1000);
 			$(".oneTry").hide();
 			$(".moreTries").fadeIn(1000);
 			$("#winNum").text(numGuesses);
 			newGame();
-		} else if (oldDiff > diffGuess) {
-			$("#feedback").text("You are getting warmer!");
+		} else if ((oldDiff > diffGuess) && ((guess >= (randomNum + 50)) || (guess <= (randomNum - 50)))) {
+			$("#feedback").text("You are ice cold, but getting warmer!");
+			console.log("1");
+		} else if ((oldDiff > diffGuess) && ((guess >= (randomNum + 30)) || (guess <= (randomNum - 30)))) {
+			$("#feedback").text("You are cold, but getting warmer!");
+			console.log("2");
+		} else if ((oldDiff > diffGuess) && ((guess >= (randomNum + 20)) || (guess <= (randomNum - 20)))) {
+			$("#feedback").text("You are warm, and getting warmer!");
+			console.log("3");
+		} else if ((oldDiff > diffGuess) && ((guess >= (randomNum + 10)) || (guess <= (randomNum - 10)))) {
+			$("#feedback").text("You are hot, and getting hotter!");
+			console.log("4");
+		} else if ((oldDiff > diffGuess) && ((guess >= (randomNum + 1)) || (guess <= (randomNum - 1)))) {
+			$("#feedback").text("You are very hot, and getting hotter!");
+			console.log("11");
+		} else if ((oldDiff < diffGuess) && ((guess >= (randomNum + 50)) || (guess <= (randomNum - 50)))) {
+			$("#feedback").text("You are ice cold, and getting colder!");
+			console.log("5");
+		} else if ((oldDiff < diffGuess) && ((guess >= (randomNum + 30)) || (guess <= (randomNum - 30)))) {
+			$("#feedback").text("You are cold, and getting colder!");
+			console.log("6");
+		} else if ((oldDiff < diffGuess) && ((guess >= (randomNum + 20)) || (guess <= (randomNum - 20)))) {
+			$("#feedback").text("You are warm, but getting colder!");
+			console.log("7");
+		} else if ((oldDiff < diffGuess) && ((guess >= (randomNum + 10)) || (guess <= (randomNum - 10)))) {
+			$("#feedback").text("You are hot, but getting colder!");
+			console.log("8");
+		} else if ((oldDiff < diffGuess) && ((guess >= (randomNum + 1)) || (guess <= (randomNum - 1)))) {
+			$("#feedback").text("You are very hot, but getting colder!");
+			console.log("12");
 		} else if (guess == oldGuess) {
 			$("#feedback").text("That was the same number, genius.");
-		} else {
-			$("#feedback").text("You are getting colder!");
+			console.log("9");
 		}
+
 	}
-
 }
-
 
 $(document).ready(function(){
 	newGame();
@@ -64,7 +94,7 @@ $(document).ready(function(){
 	$("form").on("click", "#guessButton", function() {
 		var guess = $(this).prev().val();
 		$(this).prev().val("");
-		console.log(guess);
+		console.log("guess: " + guess);
 		trueInt = parseInt(guess);
 		noNum = isNaN(parseFloat(trueInt));
 		noDec = guess % 1;
@@ -81,8 +111,9 @@ $(document).ready(function(){
 		$("#count").text(numGuesses);
 		oldDiff = diffGuess;
 		diffGuess = Math.abs(randomNum - guess);
-		console.log(diffGuess);
-		console.log(oldDiff);
+		var checkDifferenceGuesss = (randomNum - guess);
+		console.log("checkDifferenceGuesss: " + checkDifferenceGuesss);
+		
 		guessResponse(guess);
 		oldGuess = guess;
 		
